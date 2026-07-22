d3deb189e1385cec0b6b404db28dcd5e086ba164
Skalyan <kalyansurya403@gmail.com>
2026-07-22 15:50:15 +0000

    Fixed the import

---
+++ src/com/build/boutique/Constants.groovy
@@ -0,0 +1,13 @@
+package com.build
+
+class Constants {
+
+    static final List BACKEND_SERVICES = [
+        "auth",
+        "gateway",
+        "orders",
+        "order-service",
+        "product-service",
+        "user-service"
+    ]
+}
