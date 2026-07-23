c1e7853a9752d2c8d77da3bc8c1b3e72ba907832
Skalyan <kalyansurya403@gmail.com>
2026-07-23 18:04:43 +0000

    Updated the env exit-code --> exitCode

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -4,22 +4,23 @@ def call(Map config = [:]) {
 
     def severity = config.severity ?: "CRITICAL,HIGH"
 
+    //def exitCode = config.exitCode ?: 1
+
+
     if (!image) {
         error "Trivy scan failed: Image name is required"
     }
 
     echo "=========================================="
     echo "Running Trivy Image Scan"
-    echo "Image    : ${image}"
-    echo "Severity : ${severity}"
-    echo "Exit Code: ${exitCode} "
+    echo "Image     : ${image}"
+    echo "Severity  : ${severity}"
     echo "=========================================="
-
 
     sh """
         trivy image \
         --severity ${severity} \
-        --exit-code ${exitCode} \
+        --exit-code 0 \
         --no-progress \
         ${image}
     """
